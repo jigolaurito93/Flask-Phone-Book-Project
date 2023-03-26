192741634d283b2417cf3f0791f81a962efd4c93
Jose Laurito <jigolaurito93@yahoo.com>
2023-03-26 09:45:47 -0500

    created flashed message

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
 from app.forms import SignUpForm
 
 @app.route("/")
@@ -13,6 +13,16 @@ def add_phone():
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     form = SignUpForm()
+    if form.validate_on_submit():
+        print('Successfully Signed Up..')
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+        print(first_name, last_name, email, username, password)
+        flash(f"Thank you {first_name} for signing up!, "success")
+        return redirect(url_for('index'))
     return render_template('signup.html', form=form)
 
 @app.route("/login")
