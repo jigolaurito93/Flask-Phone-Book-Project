1750cc2415eb07353d14802cfdc04a6d2ec8487b
Jose Laurito <jigolaurito93@yahoo.com>
2023-03-26 09:06:33 -0500

    fixed signup page

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import Stringfield, PasswordField, Submitfield, Emailfield
+from wtforms import StringField, PasswordField, SubmitField, EmailField, TextAreaField
 from wtforms.validators import InputRequired, EqualTo
 
 class SignUpForm(FlaskForm):
-    first_name = Stringfield('First Name', validators=[InputRequired()])
-    last_name = Stringfield('Last Name', validators=[InputRequired()])
-    username = Stringfield('Username', validators=[InputRequired()])
-    email = Emailfield('Email', validators=[InputRequired()])
+    first_name = StringField('First Name', validators=[InputRequired()])
+    last_name = StringField('Last Name', validators=[InputRequired()])
+    username = StringField('Username', validators=[InputRequired()])
+    email = EmailField('Email', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
-    confirm_pass = PasswordField('Password', validators=[InputRequired(), EqualTo('password')])
-    submit = Submitfield('Sign Up')
+    confirm_pass = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+    
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template
+from app.forms import SignUpForm
 
 @app.route("/")
 def index():
@@ -9,9 +10,10 @@ def index():
 def add_phone():
     return render_template('add_phone.html')
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    return render_template('signup.html')
+    form = SignUpForm()
+    return render_template('signup.html', form=form)
 
 @app.route("/login")
 def login():
