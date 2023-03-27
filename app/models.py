9082e7db233c60b95a975521cc0958511d2f776f
Jose Laurito <jigolaurito93@yahoo.com>
2023-03-27 03:23:32 -0500

    fixed home page

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(75), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
+    contacts = db.relationship('Contact', backref='user')
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.password = generate_password_hash(kwargs.get('password'))
@@ -24,12 +24,13 @@ class User(db.Model, UserMixin):
     def check_password(self, password_guess):
         return check_password_hash(self.password, password_guess)
 
-class Address(db.Model):
+class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     phone_number = db.Column(db.String(15), nullable=False) # (###) ###-####
     address = db.Column(db.String(100))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) # SQL - FOREIGN KEY(user_id) REFERENCES user(id)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -42,3 +43,14 @@ class Address(db.Model):
 @login.user_loader
 def get_a_user_by_id(user_id):
     return db.session.get(User, user_id)
+
+def Update(self, first_name, last_name, address, phone_number):
+    self.first_name = first_name
+    self.last_name = last_name
+    self.address = address
+    self.phone_number = phone_number
+    db.session.commit()
+
+def Delete(self):
+    db.session.delete(self)
+    db.session.commit()
--- app/routes.py
+++ app/routes.py
@@ -1,28 +1,24 @@
 from app import app, db
 from flask import render_template, redirect, url_for, flash
 from app.forms import SignUpForm, LoginForm, PhoneForm
-from app.models import User, Address
-from flask_login import login_user, logout_user, login_required
+from app.models import User, Contact
+from flask_login import login_user, logout_user, login_required, current_user
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/add-phone", methods=["GET", "POST"])
+@app.route('/home')
 @login_required
-def add_phone():
-    form = PhoneForm()
-    # Check if the form was submitted and is valid
-    if form.validate_on_submit():
-        first = form.first_name.data
-        last = form.last_name.data
-        address = form.address.data
-        phone = form.phone_number.data
-        print(first, last, address, phone)
-        new_contact = Address(first_name=first, last_name=last, address=address, phone_number=phone)
-        flash(f"{new_contact.first_name} {new_contact.last_name} has been added to the phone book", "success")
-        return redirect(url_for('home'))
-    return render_template('add_phone.html', form=form)
+def home():
+    posts = Contact.query.all()
+    if current_user.is_authenticated:
+        contacts=Contact.query.filter(Contact.user_id == current_user.id).all()
+        return render_template('home.html', contacts=contacts)
+    else:
+        return render_template('home.html', contacts=[])
+    
+
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
@@ -45,7 +41,8 @@ def signup():
         new_user = User(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
         flash(f"Thank you {new_user.username} for signing up!", "success")
         return redirect(url_for('index'))
-    return render_template('signup.html', form=form)
+    return render_template('signup.html', form=form)  
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -68,13 +65,80 @@ def login():
         
     return render_template('login.html', form=form)
 
+
+
+
+@app.route("/add-phone", methods=["GET", "POST"])
+@login_required
+def add_phone():
+    form = PhoneForm()
+    # Check if the form was submitted and is valid
+    if form.validate_on_submit():
+        first = form.first_name.data
+        last = form.last_name.data
+        address = form.address.data
+        phone_number = form.phone_number.data
+        user_id = current_user.id
+
+        print(first, last, address, phone_number)
+        new_contact = Contact(first_name=first, last_name=last, address=address, phone_number=phone_number, user_id= user_id) 
+        flash(f"{new_contact.first_name} {new_contact.last_name} has been added to the phone book", "success")
+        return redirect(url_for('home'))
+    return render_template('add_phone.html', form=form)
+
+
+@app.route('/contacts/<contact_id>')
+def view_single_contact(contact_id):
+    contact = Contact.query.get_or_404(contact_id) # SELECT * FROM post WHERE id = post_id  --(post_id comes from the URL)
+    return render_template('single_contact.html', contact=contact)
+
+
+
+#edit_contact page is where logged in user can edit contact that belongs to them
+@app.route('/edit-contacts/<contact_id>', methods=["GET", "POST"])
+@login_required
+def edit_contact(contact_id):
+    contact_to_edit = Contact.query.get_or_404(contact_id)
+    if current_user != contact_to_edit.user:
+        flash("You do not have permission to edit this contact", "danger")
+        return redirect(url_for('home'))
+    form = PhoneForm()
+    if form.validate_on_submit():
+        # Get form data
+        new_first_name = form.first_name.data
+        new_last_name = form.last_name.data
+        address = form.address.data
+        phone_number = form.phone_number.data
+        # update the post to edit with the form data
+        contact_to_edit.update(first_name=new_first_name, last_name=new_last_name, address=address, phone_number=phone_number)
+
+        flash(f'{contact_to_edit.email} has been updated', 'primary')
+        return redirect(url_for('view_single_contact', contact_id=contact_to_edit.id))
+
+    return render_template('edit_contact.html', contact=contact_to_edit, form=form)
+
+
+
+
+
+@app.route('/delete-contacts/<contact_id>')
+@login_required
+def delete_contact(contact_id):
+    contact_to_delete = Contact.query.get_or_404(contact_id)
+    if current_user != contact_to_delete.user:
+        flash("You do not have permission to delete this contact", "danger")
+        return redirect(url_for('home'))
+    contact_to_delete.delete()
+    flash(f'Entry for {contact_to_delete.first_name} {contact_to_delete.last_name} has been deleted', 'info')
+    return redirect(url_for('home'))
+
+
+
+
 @app.route('/logout')
 def logout():
     logout_user()
     flash("You have logged out", "info")
     return redirect(url_for('index'))
 
-@app.route('/home')
-@login_required
-def home():
-    return render_template('home.html')
+
